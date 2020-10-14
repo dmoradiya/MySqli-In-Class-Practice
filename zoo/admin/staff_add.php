8d76d17c8609f71d7d92683e87606b3d0268aabe
Tammy Valgardson <tammalee@gmail.com>
2020-10-14 14:09:31 -0600

    staff_add page added to project

---
+++ zoo/admin/staff_add.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoo - Add Staff</title>
+</head>
+<body>
+    <h1>Add a staff member</h1>
+
+</body>
+</html>
