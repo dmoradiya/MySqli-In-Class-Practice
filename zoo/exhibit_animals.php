8479ef1c2ebd4b0fa9b324819abeaf56670d0d18
Tammy Valgardson <tammalee@gmail.com>
2020-10-14 13:53:10 -0600

    Get exhibit information and output to screen

--- zoo/exhibit_animals.php
+++ zoo/exhibit_animals.php
@@ -3,6 +3,9 @@
 
     $exhibit_id = null;
     $animals = "";
+    $exhibit_name = "";
+    $exhibit_description = "";
+    $message = "";
 
     if( !isset( $_GET['exhibit_id'] ) || $_GET['exhibit_id'] === "" ) {
         echo "You have reached this page by mistake.";
@@ -17,6 +20,10 @@
     WHERE ExhibitID = $exhibit_id
     ORDER BY Name ASC";
 
+    $exhibit_sql = "SELECT ExhibitName, ExhibitDescription
+    FROM Exhibit
+    WHERE ExhibitID = $exhibit_id";
+
     $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
     if( $connection->connect_errno ) {
         die("Connection failed: ". $connection->connect_error);
@@ -27,20 +34,39 @@
         exit();
     }
 
-    while( $animal = $animal_result->fetch_assoc() ) {
-        // echo '<pre>';
-        // print_r($animal);
-        // echo '</pre>';
-        $animals .= sprintf('
-        <h3>%s - %s</h3>
-        <p>%s</p>',
-        $animal['Name'],
-        $animal['CommonName'],
-        $animal['ScientificName'],
-        );
-
+    if( !$exhibit_result = $connection->query($exhibit_sql) ) {
+        echo "Something went wrong with the exhibit query";
+        exit();
     }
 
+    $connection->close();
+
+    if( 0 === $animal_result->num_rows ) {
+        $message = "<p>There are no animals on this exhibit</p>";
+    } else {
+        while( $animal = $animal_result->fetch_assoc() ) {
+            $animals .= sprintf('
+            <h3>%s - %s</h3>
+            <p>%s</p>',
+            $animal['Name'],
+            $animal['CommonName'],
+            $animal['ScientificName'],
+            );
+        }
+    }
+    
+
+    if( 0 === $exhibit_result->num_rows ) {
+        $message .= "<p>There is no exhibit with that ID</p>";
+    } else  {
+        while( $exhibit = $exhibit_result->fetch_assoc() ) {
+            $exhibit_name = $exhibit['ExhibitName'];
+            $exhibit_description = $exhibit['ExhibitDescription'];
+        }
+    }
+
+    
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,8 +76,9 @@
     <title>TechCareers Exhibit Animals</title>
 </head>
 <body>
-    <h1>Animals in the Exhibit Name Exhibit</h1>
-    <p>Exhibit Description</p>
+    <h1>Animals in the <?php echo $exhibit_name; ?> Exhibit</h1>
+    <p><?php echo $exhibit_description; ?></p>
+    <?php echo $message; ?>
     <?php echo $animals; ?>
 </body>
 </html>
