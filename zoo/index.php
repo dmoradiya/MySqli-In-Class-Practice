52c208c9a8274b129a4de71ff06891f5f6eff4c1
Tammy Valgardson <tammalee@gmail.com>
2020-10-14 11:36:24 -0600

    Number of Exhibits Altered select statement to only select exhibits where now is between the start and end dates Created number_of_exhibits variable and populated it with num_rows from result

--- zoo/index.php
+++ zoo/index.php
@@ -3,16 +3,19 @@
 
     // Create connection
     $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
-    
+    $number_of_exhibits = 0;
+
     // Did we have errors connecting?
     if ($connection->connect_error) {
         die('Connection failed: ' . $connection->connect_error);
     }
     
-    $sql = "SELECT * FROM Exhibit";
+    $sql = "SELECT * FROM Exhibit WHERE NOW() BETWEEN `StartDate` AND `EndDate`";
     
     $result = $connection->query($sql);
-        
+    
+    $number_of_exhibits = $result->num_rows;
+
     if( $result->num_rows > 0 ) {
         
         while( $row = $result->fetch_assoc() ){
@@ -28,3 +31,17 @@
     
     $connection->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechCareers Zoo</title>
+</head>
+<body>
+    <h1>Welcome to Tech Careers Zoo</h1>
+    <h2>Exhibits</h2>
+    <p>We currently have <?php echo $number_of_exhibits; ?> exhibit(s) for you to visit</p>
+</body>
+</html>
