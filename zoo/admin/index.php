640cf5bfba3c26dbf33571b55aa42ef3d29fb1a6
Tammy Valgardson <tammalee@gmail.com>
2020-10-14 15:06:24 -0600

    Added admin index. Added animal_add. Added a menu to index file.

---
+++ zoo/admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoo - Add Staff</title>
+</head>
+<body>
+    <h1>Admin menu</h1>
+    <ul>
+        <li><a href="animal_add.php">Add animal</a></li>
+        <li><a href="staff_list.php">Staff listing</a></li>
+        <li><a href="staff_add.php">Add staff member</a></li>
+    </ul>
+</body>
+</html>
