9fa9aa0465c3bca469222b0b014015ed1c3f3094
Tammy Valgardson <tammalee@gmail.com>
2020-10-14 14:15:08 -0600

    added form to staff_add file

--- zoo/admin/staff_add.php
+++ zoo/admin/staff_add.php
@@ -1,4 +1,5 @@
 <?php
+    require '../constants.php';
 
 ?>
 <!DOCTYPE html>
@@ -10,6 +11,18 @@
 </head>
 <body>
     <h1>Add a staff member</h1>
-
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+    <p>
+        <label for="first_name">First Name</label>
+        <input type="text" name="first_name" id="first_name">
+    </p>
+    <p>
+        <label for="last_name">Last Name</label>
+        <input type="text" name="last_name" id="last_name">
+    </p>
+    <p>
+        <input type="submit" value="Add new staff member">
+    </p>
+    </form>
 </body>
 </html>
