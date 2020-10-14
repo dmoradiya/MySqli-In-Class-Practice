bf6a2fc9c32cdd9089c461c39330965294b11d96
Tammy Valgardson <tammalee@gmail.com>
2020-10-14 13:40:27 -0600

    Output animals on exhibit_animals.php

--- zoo/exhibit_animals.php
+++ zoo/exhibit_animals.php
@@ -1,11 +1,45 @@
 <?php
+    require 'constants.php';
+
     $exhibit_id = null;
+    $animals = "";
 
     if( !isset( $_GET['exhibit_id'] ) || $_GET['exhibit_id'] === "" ) {
         echo "You have reached this page by mistake.";
         exit();
     }
     $exhibit_id = $_GET['exhibit_id'];
+
+    $animal_sql = "SELECT Name, CommonName, ScientificName
+    FROM Animal
+    INNER JOIN ExhibitAnimal USING(AnimalID)
+    INNER JOIN Species USING(SpeciesID)
+    WHERE ExhibitID = $exhibit_id
+    ORDER BY Name ASC";
+
+    $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
+    if( $connection->connect_errno ) {
+        die("Connection failed: ". $connection->connect_error);
+    }
+
+    if( !$animal_result = $connection->query($animal_sql) ) {
+        echo "Something went wrong with the animal query";
+        exit();
+    }
+
+    while( $animal = $animal_result->fetch_assoc() ) {
+        // echo '<pre>';
+        // print_r($animal);
+        // echo '</pre>';
+        $animals .= sprintf('
+        <h3>%s - %s</h3>
+        <p>%s</p>',
+        $animal['Name'],
+        $animal['CommonName'],
+        $animal['ScientificName'],
+        );
+
+    }
 
 ?>
 <!DOCTYPE html>
@@ -18,7 +52,6 @@
 <body>
     <h1>Animals in the Exhibit Name Exhibit</h1>
     <p>Exhibit Description</p>
-    <h3>Animal Name - Common Name</h3>
-    <p>Scientific Name</p>
+    <?php echo $animals; ?>
 </body>
 </html>
