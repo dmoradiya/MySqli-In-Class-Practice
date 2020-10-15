c1d6dffc31e9e74e028bdfb70dc8192ea61412d9
Tammy Valgardson <tammalee@gmail.com>
2020-10-15 11:43:44 -0600

    sanitize staff_id. query staff table

--- zoo/admin/staff_delete.php
+++ zoo/admin/staff_delete.php
@@ -1,5 +1,29 @@
 <?php 
 
+    require '../constants.php';
+    $staff_id = null;
+
+    $connection = new MySQLi(HOST, USER, PASSWORD, DATABASE);
+    if( $connection->connect_errno ) {
+        die("Connection failed:" . $connection->connect_error);
+    }
+    if( !isset($_GET['staff_id']) || $_GET['staff_id'] === "" ) {
+        exit("You have reached this page by mistake");
+    }
+    if( filter_var($_GET['staff_id'], FILTER_VALIDATE_INT) ) {
+        $staff_id = $_GET['staff_id'];
+    } else {
+        exit("An incorrect value for Staff ID was used");
+    }
+    $sql = "SELECT * FROM Staff WHERE StaffID=$staff_id";
+    $result = $connection->query($sql);
+    if( !$result ) {
+        exit("There was a problem fetching results");
+    }
+    if( 0 === $result->num_rows ) {
+        exit("The staff id provided did not match anyone in the database");
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
