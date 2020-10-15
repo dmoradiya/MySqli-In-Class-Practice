59a311cbc661c3088fea32edc445533fd4f703d5
Tammy Valgardson <tammalee@gmail.com>
2020-10-15 10:28:31 -0600

    post updates staff member in database

--- zoo/admin/staff_edit.php
+++ zoo/admin/staff_edit.php
@@ -1,6 +1,30 @@
 <?php
     require '../constants.php';
     $staff_id = null;
+    $first_name = null;
+    $last_name = null;
+    $message = null;
+    
+    $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
+    if( $connection->connect_errno ) {
+        die('Connection failed:' . $connection->connect_error);
+    }
+
+    if( $_POST ) {
+        if( $statement = $connection->prepare("UPDATE Staff SET FirstName=?, LastName=? WHERE StaffID=?")) {
+            if( $statement->bind_param("ssi", $_POST['first_name'], $_POST['last_name'], $_POST['staff_id']) ) {
+                if( $statement->execute() ) {
+                   $message = "You have updated successfully"; 
+                } else {
+                    exit("There was a problem with the execute");
+                }
+            } else {
+                exit("There was a problem with the bind_param");
+            }
+        } else {
+            exit("There was a problem with the prepare statement");
+        }
+    }
 
     // If we don't have a staff id, do not continue
     if( !isset($_GET['staff_id']) || $_GET['staff_id'] === "" ) {
@@ -14,6 +38,22 @@
         exit("An incorrect value was passed");
     }
 
+    $sql = "SELECT * FROM Staff where StaffID = $staff_id";
+    
+    $result = $connection->query($sql);
+    if( !$result ) {
+        exit('There was a problem fetching results');
+    }
+    if( 0 === $result->num_rows ) {
+        exit("There was no staff with that ID");
+    }
+
+    while( $row = $result->fetch_assoc() ) {
+        $first_name = $row['FirstName'];
+        $last_name = $row['LastName'];
+    }
+    $connection->close();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,17 +65,19 @@
 <body>
     <?php include 'admin_menu.php' ?>
     <h1>Edit Staff Member</h1>
+    <?php if($message) echo $message; ?>
     <form action="#" method="POST" enctype="multipart/form-data">
     <p>
         <label for="first_name">First Name</label>
-        <input type="text" name="first_name" id="first_name">
+        <input type="text" name="first_name" id="first_name" value="<?php echo $first_name; ?>">
     </p>
     <p>
         <label for="last_name">Last Name</label>
-        <input type="text" name="last_name" id="last_name">
+        <input type="text" name="last_name" id="last_name" value="<?php echo $last_name; ?>">
+        <input type="hidden" name="staff_id" value="<?php echo $staff_id; ?>">
     </p>
     <p>
-        <input type="submit" value="Add new staff member">
+        <input type="submit" value="Submit edits">
     </p>
     </form>
 </body>
