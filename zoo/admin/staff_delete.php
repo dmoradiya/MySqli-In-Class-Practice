182d0cc38bf595828b12b3ca2165a8319e5e3a24
Tammy Valgardson <tammalee@gmail.com>
2020-10-15 11:50:27 -0600

    show form on page load but not after post

--- zoo/admin/staff_delete.php
+++ zoo/admin/staff_delete.php
@@ -4,31 +4,39 @@
     $staff_id = null;
     $first_name = null;
     $last_name = null;
+    $show_form = true;
 
     $connection = new MySQLi(HOST, USER, PASSWORD, DATABASE);
     if( $connection->connect_errno ) {
         die("Connection failed:" . $connection->connect_error);
     }
-    if( !isset($_GET['staff_id']) || $_GET['staff_id'] === "" ) {
-        exit("You have reached this page by mistake");
-    }
-    if( filter_var($_GET['staff_id'], FILTER_VALIDATE_INT) ) {
-        $staff_id = $_GET['staff_id'];
-    } else {
-        exit("An incorrect value for Staff ID was used");
-    }
-    $sql = "SELECT * FROM Staff WHERE StaffID=$staff_id";
-    $result = $connection->query($sql);
-    if( !$result ) {
-        exit("There was a problem fetching results");
-    }
-    if( 0 === $result->num_rows ) {
-        exit("The staff id provided did not match anyone in the database");
+
+    if( !$_POST ) {
+        if( !isset($_GET['staff_id']) || $_GET['staff_id'] === "" ) {
+            exit("You have reached this page by mistake");
+        }
+        if( filter_var($_GET['staff_id'], FILTER_VALIDATE_INT) ) {
+            $staff_id = $_GET['staff_id'];
+        } else {
+            exit("An incorrect value for Staff ID was used");
+        }
+        $sql = "SELECT * FROM Staff WHERE StaffID=$staff_id";
+        $result = $connection->query($sql);
+        if( !$result ) {
+            exit("There was a problem fetching results");
+        }
+        if( 0 === $result->num_rows ) {
+            exit("The staff id provided did not match anyone in the database");
+        }
+
+        while( $row = $result->fetch_assoc() ) {
+            $first_name = $row['FirstName'];
+            $last_name = $row['LastName'];
+        }
     }
 
-    while( $row = $result->fetch_assoc() ) {
-        $first_name = $row['FirstName'];
-        $last_name = $row['LastName'];
+    if( $_POST ) {
+        $show_form = false;
     }
 
 
@@ -43,10 +51,14 @@
 <body>
     <?php include 'admin_menu.php'; ?>
     <h1>Remove Staff Member</h1>
-    <form action="#" method="POST" enctype="multipart/form-data">
-        <p>Are you certain you want to remove <?php echo $first_name . ' ' . $last_name; ?></p>
-        <input type="hidden" name="staff_id" value="<?php echo $staff_id; ?>">
-        <input type="submit" value="Yes, remove staff member">
-    </form>
+    <?php if( $show_form ): ?>
+        <form action="#" method="POST" enctype="multipart/form-data">
+            <p>Are you certain you want to remove <?php echo $first_name . ' ' . $last_name; ?></p>
+            <input type="hidden" name="staff_id" value="<?php echo $staff_id; ?>">
+            <input type="submit" value="Yes, remove staff member">
+        </form>
+    <?php else: ?>
+        <p>I am a message</p>
+    <?php endif; ?>
 </body>
 </html>
